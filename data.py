9074ba3f7e430cec5fa0d66f9c7cc0f2b60de3d3
Bhaskar-Malik <kmalikbmalik@gmail.com>
2025-08-20 12:40:56 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+#I made two ways two get data
 def loop():
          P_A=pd.read_excel(r"C:\Users\hp\Desktop\PvsA.xlsx")
          for i in range(0,54,21):
@@ -11,4 +12,10 @@ def subplot():
 
 print("1.Loop statement(press 1)")
 print("2.Subplot(press 2)")
-
+ch=int(input("which one do you want to use"))
+if ch==1:
+         loop()
+elif ch==2:
+         subplot()
+else:
+         print("Please enter a valid choice")
