b9e6ee69bd6a3af9baddb7a34ccc1f06439e44d7
Bhaskar Malik <kmalikbmalik@gmail.com>
2025-08-22 15:20:51 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 #I made two ways two get data
-def loop():
-         P_A=pd.read_excel(r"C:\Users\hp\Desktop\PvsA.xlsx")
-         for i in range(0,54,21):
-                   print(P_A.iloc[i:i+25])
+def loop():#In this I made a loop which takes in i has input for loop 
+         P_A=pd.read_excel(r"C:\Users\hp\Desktop\PvsA.xlsx")#I read excel
+         for i in range(0,54,21):#A gap of 21 is done so i=1,22..
+                   print(P_A.iloc[i:i+25])#iloc in integer location which is used tot ake data from the excel
 
 def subplot():
-        df=pd.read_excel("C:\\Users\\hp\\Desktop\\PvsA.xlsx",'Sheet1')
+        df=pd.read_excel("C:\\Users\\hp\\Desktop\\PvsA.xlsx",'Sheet1')#Reads the given sheet
         print(df)
-
+#Made some choice based if-else
 print("1.Loop statement(press 1)")
 print("2.Subplot(press 2)")
 ch=int(input("which one do you want to use"))
